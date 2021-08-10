c72b265694e54c3d1aa98cb110be60c841cdf40e
Aliciahdzg <hernndez.alicia4@gmail.com>
2021-08-10 13:52:17 -0500

    se agrega addEventListener al boton submit aun no imprime

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 // Este es el punto de entrada de tu aplicacion
 //import { myFunction } from './lib/index.js';
 //myFunction();
+const form = document.getElementById('signin');
+form.addEventListener('click', (e) => {
+    e.preventDefault;
+    const button = document.getElementById('submitBTN').value;
+    const email = document.getElementById('emailUser').value;
+    const password = document.getElementById('passwordUser').value;
+    console.log('hola');
+});
