67d2e9d2164fbd0800157e4c27bab2555f71d240
Aliciahdzg <hernndez.alicia4@gmail.com>
2021-08-11 00:39:13 -0500

    creacion form html con los input email y password y el boton de submit, creacion del addeventlistener para guardar los datos al igual que la creacion del proyecto en el firebase y agregadas las lineas necesarias para en el html, se comprobo que los datos ingresados se almacenan en el firebase

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 // Este es el punto de entrada de tu aplicacion
-//import { myFunction } from './lib/index.js';
-//myFunction();
-const form = document.getElementById('signin');
+// import { myFunction } from './lib/index.js';
+// myFunction();
+const form = document.querySelector('button[type=submit]');
 form.addEventListener('click', (e) => {
-    e.preventDefault;
-    const button = document.getElementById('submitBTN').value;
+    e.preventDefault();
+    // const button = document.getElementById('submitBTN').value;
     const email = document.getElementById('emailUser').value;
     const password = document.getElementById('passwordUser').value;
-    console.log('hola');
+    alert(email, password);
+
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            console.log('esta es la promesa');
+        });
 });
