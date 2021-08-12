b0638c047dbd06139eaaacdf07d516a71b35602c
Aliciahdzg <hernndez.alicia4@gmail.com>
2021-08-11 21:48:45 -0500

    cambios minimos en acomodo

--- src/main.js
+++ src/main.js
@@ -3,15 +3,15 @@
 // myFunction();
 const form = document.querySelector('button[type=submit]');
 form.addEventListener('click', (e) => {
-    e.preventDefault();
-    // const button = document.getElementById('submitBTN').value;
-    const email = document.getElementById('emailUser').value;
-    const password = document.getElementById('passwordUser').value;
-    alert(email, password);
+  e.preventDefault();
+  // const button = document.getElementById('submitBTN').value;
+  const email = document.getElementById('emailUser').value;
+  const password = document.getElementById('passwordUser').value;
+  alert(email, password);
 
-    auth
-        .createUserWithEmailAndPassword(email, password)
-        .then(userCredential => {
-            console.log('esta es la promesa');
-        });
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(userCredential => {
+      console.log('esta es la promesa');
+    });
 });
