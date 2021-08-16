d7d0e5230909639d59b994208f0fd7c5f46557eb
Aliciahdzg <hernndez.alicia4@gmail.com>
2021-08-16 00:05:28 -0500

    Updates

--- main.js
+++ main.js
@@ -3,36 +3,34 @@
 // myFunction();
 const formSignUp = document.querySelector('button[type=submit]');
 formSignUp.addEventListener('click', (e) => {
-  e.preventDefault();
-  // const button = document.getElementById('submitBTN').value;
-  const email = document.getElementById('emailUser').value;
-  const password = document.getElementById('passwordUser').value;
-  console.log(email, password);
+    e.preventDefault();
+    // const button = document.getElementById('submitBTN').value;
+    const email = document.getElementById('emailUser').value;
+    const password = document.getElementById('passwordUser').value;
+    console.log(email, password);
 
-  auth
-    .createUserWithEmailAndPassword(email, password)
-    .then(userCredential => {
-      console.log('esta es la promesa');
-    });
-  
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            console.log('esta es la promesa');
+        });
+
 });
 
 const formSignIn = document.querySelector('#submitLogin');
 formSignIn.addEventListener('click', (e) => {
-  e.preventDefault();
-  // const button = document.getElementById('submitBTN').value;
-  const email = document.getElementById('emailLogin').value;
-  const password = document.getElementById('passwordLogin').value;
-  console.log(email, password);
+    e.preventDefault();
+    // const button = document.getElementById('submitBTN').value;
+    const email = document.getElementById('emailLogin').value;
+    const password = document.getElementById('passwordLogin').value;
+    console.log(email, password);
 
-  auth
-    .signInWithEmailAndPassword(email, password)
-    .then(userCredential => {
-      console.log('ya iniciaste sesion');
-    });
-  
+    auth
+        .signInWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            console.log('ya iniciaste sesion');
+        });
 });
-
 
 /*const providerGoogle = document.querySelector('#registerGoogle');
 providerGoogle.addEventListener('click', e => {
