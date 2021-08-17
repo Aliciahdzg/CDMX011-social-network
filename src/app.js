34eb89ec3b165950c9abd6f7e20c0807dda9a56d
Karli <nasabisan@gmail.com>
2021-08-17 10:16:25 -0500

    Se comienzan a reestructurar las rutas y sus componentes

--- src/app.js
+++ src/app.js
@@ -1,33 +1,38 @@
 const routes = {
-  '/': home,
-  '/sigup': signup,
-  '/login': login,
+    '/': home,
+    '/login': login,
+    '/signup': signup,
 };
 
-const rootDiv = document.getElementById('root');
+const rootDiv = document.querySelector('#root');
 rootDiv.innerHTML = routes[window.location.pathname];
 
 const onNavigate = (pathname) => {
-  window.history.pushState({},
-    pathname,
-    window.location.origin + pathname);
-  rootDiv.innerHTML = routes[pathname];
+    window.history.pushState({},
+        pathname,
+        window.location.origin + pathname
+    )
+    rootDiv.innerHTML = routes[pathname]
 };
 
 const homeRoute = document.querySelector('#home');
+
+function homeCharge() {
+    return onNavigate('/');
+}
+
 homeRoute.addEventListener('click', () => {
-  onNavigate('/');
+    onNavigate('/');
 });
 const loginRoute = document.querySelector('#login');
 loginRoute.addEventListener('click', () => {
-  onNavigate('/login');
+    onNavigate('/login');
 });
 const signupRoute = document.querySelector('#signup');
 signupRoute.addEventListener('click', () => {
-  onNavigate('/signup');
+    onNavigate('/signup');
 });
 
-
 window.onpopstate = () => {
-  rootDiv.innerHTML = routes[window.location.pathname];
+    rootDiv.innerHTML = routes[window.location.pathname];
 };
--- src/login.js
+++ src/login.js
@@ -1,3 +1,17 @@
 const login = `
-<h1>Soy el Login</h1>
+<section class="grid-father ">
+    <section class="container ">
+        <form id="login ">
+
+        <input type="email " id="emailLogin " class="form-elements " placeholder="Correo " name="Email " required />
+
+        <input type="password " id="passwordLogin " class="form-elements " placeholder="Contraseña " name="password-user " required>
+        <button type="submit " value="enviar " id="submitLogin " class="login-button ">Inicia Sesión</button>
+        </form>
+        <p>ó</p>
+        <button type="button " id="loginGoogle " class="login-google "> 
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg ">
+        Inicia Sesión con Google</button>
+    </section> 
+</section>
 `;
