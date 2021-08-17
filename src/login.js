ae43a3b2b6ef5f7000bc269b20f611a312b05ab8
Gemma <gemminew@gmail.com>
2021-08-17 13:47:02 -0500

    modificaciones en el diseño de login home y signup

--- src/login.js
+++ src/login.js
@@ -2,16 +2,17 @@ const login = `
 <section class="grid-father ">
     <section class="container ">
         <form id="login ">
-
-        <input type="email " id="emailLogin " class="form-elements " placeholder="Correo " name="Email " required />
-
+        <label>Correo</label>
+            <input type="email " id="emailLogin " class="form-elements " placeholder="Correo " name="Email " required />
+        <label>Contraseña</label>
         <input type="password " id="passwordLogin " class="form-elements " placeholder="Contraseña " name="password-user " required>
-        <button type="submit " value="enviar " id="submitLogin " class="login-button ">Inicia Sesión</button>
+        <button type="submit " value="enviar " id="submitLogin " class="signup-button ">Inicia Sesión</button>
         </form>
-        <p>ó</p>
-        <button type="button " id="loginGoogle " class="login-google "> 
-        <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg ">
-        Inicia Sesión con Google</button>
+        <p class="hr-test "><span>ó</span></p>
+        <button type="button " id="registerGoogle " class="signup-google button-google "> 
+         <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg "><div>Accede con Google</div></button>
+         <br>
+         <button type="button " id="#" class="login-in-register ">Registrate Aqui</p>
     </section> 
 </section>
 `;
